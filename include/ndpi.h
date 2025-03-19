8c6121902a488d550d9ca09b638d589d1e397a67
Christian Danizek <christian@danizek.com>
2025-03-19 21:40:05 +0100

    remove unnecessary includes

--- include/ndpi.h
+++ include/ndpi.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#include <stdint.h>
-#include <time.h>
-#include <winsock2.h>
-#include <windows.h>
-#include <ws2tcpip.h>
-
-
 namespace ndpi {
 	extern "C" {
 		#include <ndpi/ndpi_main.h>
