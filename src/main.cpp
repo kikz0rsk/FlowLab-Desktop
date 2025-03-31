1b31bb25975aa6f26e788d2ada87210cbeb19ad8
Christian Danizek <christian@danizek.com>
2025-03-31 17:19:34 +0200

    ignore sigpipe

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <signal.h>
 
 #include "proxy_service.h"
 #include "tracy/Tracy.hpp"
 
+
 int main(int argc, char *argv[]) {
+#ifdef _POSIX
+	signal(SIGPIPE, SIG_IGN);
+#endif
 	TracyNoop;
 	std::shared_ptr<ProxyService> proxyService = std::make_shared<ProxyService>();
 	QApplication a(argc, argv);
